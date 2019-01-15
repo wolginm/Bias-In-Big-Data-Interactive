b7901ab7ad6d2f57db19a9ea23f5f871c7278b0a
NCC74656 <wolginmark@gmail.com>
2019-01-15 04:43:48 -0500

    Added Slider and Color Editor

---
+++ Assets/TextFloat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextFloat : MonoBehaviour
+{
+    public Slider slider;
+    public Text text;
+    public Image img;
+
+    
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = slider.value.ToString();
+        img.color = EightBitColor((int)slider.value);
+        
+    }
+
+    private Color EightBitColor(int bit)
+    {
+        float t = (float)bit / 255;
+        return Color.HSVToRGB(t, 1, 1);
+    }
+}
