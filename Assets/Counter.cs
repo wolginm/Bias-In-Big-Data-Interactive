14a5b1a5d290f78208b31034714333c7e6c5c323
Minji Kong <mkong@udel.edu>
2019-01-17 08:26:02 +1300

    added counter

---
+++ Assets/Counter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Counter : MonoBehaviour
+{
+    public float Score = 0;
+    public float previousScore = 0;
+    public Text sscore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Score = previousScore + 1;
+            previousScore = Score;
+            Debug.Log(Score);
+            sscore.text = Score.ToString();
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Score = previousScore - 1;
+            previousScore = Score;
+            Debug.Log(Score);
+            sscore.text = Score.ToString();
+        }
+    }
+}
