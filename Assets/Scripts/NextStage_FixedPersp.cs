ab9df7e0fa96da5ef0268b73f7e0bc4ce4e13190
Minji Kong <mkong@udel.edu>
2019-01-16 23:41:37 +1300

    added buttons to switch between scenes

---
+++ Assets/Scripts/NextStage_FixedPersp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextStage_FixedPersp : MonoBehaviour
+{
+    //void OnMouseUp()
+    //{
+    //    SceneManager.LoadScene("Slider Example", LoadSceneMode.Single);
+    //}
+
+    public void changemenuscene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+}
