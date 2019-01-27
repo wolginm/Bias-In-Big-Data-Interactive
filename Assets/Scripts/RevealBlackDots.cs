672059b9c1c149b7579c20b3c5ffc829a6f8453d
Minji Kong <mkong@udel.edu>
2019-01-28 09:55:17 +1300

    background color changes in same scene

---
+++ Assets/Scripts/RevealBlackDots.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RevealBlackDots : MonoBehaviour
+{
+    public Camera cam;
+    public Button button;
+
+    private void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("Main Camera").GetComponent<Camera>();
+        cam.clearFlags = CameraClearFlags.SolidColor;
+        button.onClick.AddListener(OnClick);
+    }
+
+    public void OnClick()
+    {
+        cam.backgroundColor = Color.white;
+    }
+}
