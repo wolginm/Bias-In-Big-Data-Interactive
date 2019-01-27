39f899f49721a4d27c67a5c0d49b12c6720dd075
NCC74656 <wolginmark@gmail.com>
2019-01-27 16:20:14 -0500

    Updating Click Script

--- Assets/Scripts/RandomCircleClick.cs
+++ Assets/Scripts/RandomCircleClick.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class RandomCircleClick : MonoBehaviour
 {
     private SpriteRenderer sr;
+    public GameObject CircleParent;
+    private GameObject[] LoC;
 
     private void Start()
     {
         sr = gameObject.GetComponent<SpriteRenderer>();
+        int children = CircleParent.transform.childCount;
+        LoC = new GameObject[children];
+        for (int k = 0; k < children; k++)
+        {
+            LoC[k] = CircleParent.transform.GetChild(k).gameObject;
+            Debug.Log(LoC[k]);
+        }
+
     }
 
     private void Update()
     {
         if (Input.GetMouseButton(0))
+        {
+            Debug.Log(Input.mousePosition);
             sr.color = Color.HSVToRGB(0, 1, 1);
+        }
+            
+
     }
 }
