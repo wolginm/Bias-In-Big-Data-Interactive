3f59994dbc4c6b9e2e6ce4e1eccc5d2c1abb7e92
NCC74656 <wolginmark@gmail.com>
2019-01-27 20:55:33 -0500

    Rudimentary clicking works

--- Assets/Scripts/RandomCircleClick.cs
+++ Assets/Scripts/RandomCircleClick.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RandomCircleClick : MonoBehaviour
 {
     private SpriteRenderer sr;
     public GameObject[] LoC;
     private Camera cam;
+    public Text textCount;
+    private int counter;
     // this. is the circle parent
 
     private void Start()
     {
         Debug.Log("Hit Click Start");
         cam = Camera.main;
+        counter = 0;
     }
 
     void OnTransformChildrenChanged()
@@ -41,20 +45,26 @@ public class RandomCircleClick : MonoBehaviour
             mousePos.x = Input.mousePosition.x;
             mousePos.y = cam.pixelHeight - Input.mousePosition.y;
 
-            point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, (-1 * (mousePos.y)), cam.nearClipPlane));
+            point = cam.ScreenToWorldPoint(new Vector3(mousePos.x, (mousePos.y + Camera.main.orthographicSize + (float).5), cam.nearClipPlane));
 
             Debug.Log(point.ToString("F3"));
             Debug.Log(LoC.Length);
             foreach(GameObject go in LoC)
             {
                 Loc goLoc = go.GetComponent<Location>().thisLoc;
-                if (goLoc.click(point.x, point.y))
+                if (goLoc.click(point.x, -1 * point.y))
                 {
                     Debug.Log(goLoc.ToString);
                     Debug.Log(new Loc(Input.mousePosition.x, Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.y).ToString);
                     sr = go.GetComponent<SpriteRenderer>();
                     Debug.Log(Input.mousePosition);
                     sr.color = Color.HSVToRGB(0, 1, 1);
+                    if (go.GetComponent<VariousVar>().clicked == false)
+                    {
+                        counter++;
+                        textCount.text = counter.ToString();
+                        go.GetComponent<VariousVar>().clicked = true;
+                    }
                 }
             }
             
